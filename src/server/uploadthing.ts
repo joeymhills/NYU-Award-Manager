62e97d956aec8c0e15376aed328375ddeb23f09c
Joey Hills <joeymhills@gmail.com>
2023-06-30 12:02:10 -0400

    removed middleware functionality

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -5,27 +5,14 @@ import { clerkClient, currentUser } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next-legacy";
  
 const f = createUploadthing();
- 
-const auth = (req: NextApiRequest, res: NextApiResponse) => clerkClient // Fake auth function
- 
+  
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req, res }) => {
-      // This code runs on your server before upload
-      const user = await currentUser;
- 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new Error("Unauthorized");
- 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
+
+    .onUploadComplete(async ({ file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
  
       console.log("file url", file.url);
     }),
