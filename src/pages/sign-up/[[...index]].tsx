302c323e99896b8b729f65e843b04375628f3ec5
Joey Hills <joeymhills@gmail.com>
2023-07-14 22:07:56 -0400

    commit

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+  <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#DBA349] to-[#501685]">
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <SignUp />
+    </div>
+  </main>
+  );
 }
