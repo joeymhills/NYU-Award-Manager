b942c935b931af7286f5a3658d02a73bf9c9c0ee
Joey Hills <joeymhills@gmail.com>
2023-08-12 14:54:33 -0400

    mepw

--- src/components/Deletewindow.tsx
+++ src/components/Deletewindow.tsx
@@ -6,7 +6,7 @@ const Deletewindow = (id:props) => {
     
     async function deleteAccolade(id:props) { 
         try {
-          await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/delete`,{
+          await fetch("/api/delete",{
             body: JSON.stringify(id),
             headers: { 'Content-Type': 'Application/json'},
             method: 'POST'});
