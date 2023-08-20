860531e08413e5cbe5e0a777259af7b6c991fe3f
Joey Hills <joeymhills@gmail.com>
2023-08-20 13:00:40 -0400

    ger

--- src/components/atoms.ts
+++ src/components/atoms.ts
@@ -4,7 +4,9 @@ const accoladeFormAtom = atom(false)
 const showDetailPage = atom(false)
 const uFilter = atom("unassigned")
 const searchCallback = atom("")
+const aFilter = atom("Select a service line")
 
+export { aFilter }
 export { searchCallback }
 export { accoladeFormAtom}
 export { showDetailPage }
