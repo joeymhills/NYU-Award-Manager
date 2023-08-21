42ecaa9b308dad8d193eb9a41c026b128447517a
Joey Hills <joeymhills@gmail.com>
2023-08-21 11:44:25 -0400

    ortho

--- src/components/CreateDropdown.tsx
+++ src/components/CreateDropdown.tsx
@@ -136,6 +136,19 @@ export default function CreateDropdown() {
             <Menu.Item>
               {({ active }) => (
                 <a
+                onClick={()=> setServiceLine("Orthopedics")}
+                className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                Orthopedics 
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
                 onClick={()=> setServiceLine("Rehabilitation")}
                 className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
