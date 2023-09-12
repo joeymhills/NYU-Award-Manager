a7b017bb8ab9e9e11f36d15ccae907bb48b4a9a8
Joey Hills <joeymhills@gmail.com>
2023-09-12 19:34:44 -0400

    meow

--- src/components/HomeServiceDropdown.tsx
+++ src/components/HomeServiceDropdown.tsx
@@ -20,9 +20,9 @@ import { Fragment } from "react";
   return (
  
   <div className="flex flex-col justify-center items-center">
-  <Menu as="div" className="relative inline-block w-72 sm:w-96 text-left">
+  <Menu as="div" className="relative inline-block w-56 sm:w-96 text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md text-ellipsis bg-white px-3 py-2 text-md font-bentonreg text-gray-900 drop-shadow-md ring-1 ring-inset
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md text-ellipsis bg-white px-3 py-2 text-sm font-bentonreg text-gray-900 drop-shadow-md ring-1 ring-inset
           ring-gray-300 hover:cursor-pointer hover:bg-gray-50">
          {(serviceLine == "") ? "Select a service line" : serviceLine}
 
