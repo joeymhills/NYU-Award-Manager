d2c0f2f945694291b6b4113c2d5bfe56b46f4f04
Joey Hills <joeymhills@gmail.com>
2023-09-20 12:41:53 -0400

    middleware adjustment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: [ '/app', '/admin', '/addAward', '/', '/unauthorized', ]
+  matcher: [ '/app','/detailPage','/editAward','/adminDetailPage', '/admin','/admin2', '/addAward', '/', '/unauthorized', ]
 }
